5a2e760c3a91f64fad0a6b619e300c65109fb57f
klein994 <klein478@gmail.com>
2022-03-01 17:49:37 -0300

    mejoras en nav

--- src/components/CategoriesMenu/CategoriesMenu.jsx
+++ src/components/CategoriesMenu/CategoriesMenu.jsx
@@ -1,11 +1,35 @@
 import React from 'react';
+import {IoChevronDownOutline} from "react-icons/io5";
 
 const CategoriesMenu = () => {
     return (
         <>
-            <a href="#"
-               className="block text-lg p-4 text-white bg-blue-700 rounded md:bg-transparent md:text-white md:p-0 dark:text-white"
-               aria-current="page">Categorías</a>
+            <button id="dropdownNavbarLink" data-dropdown-toggle="dropdownNavbar"
+                    className="flex justify-between items-center py-2 pr-4 pl-3 w-full font-medium text-gray-700 border-b border-gray-100 hover:bg-gray-50 md:hover:bg-transparent md:border-0 md:hover:text-blue-700 md:p-0 md:w-auto dark:text-gray-400 dark:hover:text-white dark:focus:text-white dark:border-gray-700 dark:hover:bg-gray-700 md:dark:hover:bg-transparent">Categorías
+                <IoChevronDownOutline/>
+            </button>
+
+            <div id="dropdownNavbar"
+                 className="hidden z-10 w-44 text-base list-none bg-white rounded divide-y divide-gray-100 shadow dark:bg-gray-700 dark:divide-gray-600">
+                <ul className="py-1" aria-labelledby="dropdownLargeButton">
+                    <li>
+                        <a href="#"
+                           className="block py-2 px-4 text-sm text-gray-700 hover:bg-gray-100 dark:hover:bg-gray-600 dark:text-gray-400 dark:hover:text-white">Electrodomésticos</a>
+                    </li>
+                    <li>
+                        <a href="#"
+                           className="block py-2 px-4 text-sm text-gray-700 hover:bg-gray-100 dark:hover:bg-gray-600 dark:text-gray-400 dark:hover:text-white">Gourmet</a>
+                    </li>
+                    <li>
+                        <a href="#"
+                           className="block py-2 px-4 text-sm text-gray-700 hover:bg-gray-100 dark:hover:bg-gray-600 dark:text-gray-400 dark:hover:text-white">Vinos</a>
+                    </li>
+                </ul>
+                <div className="py-1">
+                    <a href="#"
+                       className="block py-2 px-4 text-sm text-gray-700 hover:bg-gray-100 dark:hover:bg-gray-600 dark:text-gray-400 dark:hover:text-white">Todas</a>
+                </div>
+            </div>
         </>
     );
 };
--- src/components/NavBar/NavBar.jsx
+++ src/components/NavBar/NavBar.jsx
@@ -3,20 +3,40 @@ import CartWidget from "../CartWidget/CartWidget";
 import HomeWidget from "../HomeWidget/HomeWidget";
 import LogInWidget from "../LoginWidget/LogInWidget";
 import CategoriesMenu from "../CategoriesMenu/CategoriesMenu";
-import MenuMobile from "../MenuMobile/MenuMobile";
-import LogoShop from "../LogoShop/LogoShop";
+import {IoMenu} from "react-icons/io5";
 
 const NavBar = () => {
     return (
         <div className="shadow-xl">
-            <nav className="bg-white border-gray-200 px-2 sm:px-4 py-2.5  dark:bg-black opacity-50 ">
-                <div className="container flex flex-wrap justify-between items-center p-3 mx-auto">
-                    <MenuMobile/>
-                  <LogoShop/>
-                    <div className="hidden w-full md:block md:w-auto" id="mobile-menu">
-                        <ul className="flex flex-col mt-4 md:flex-row md:space-x-8 md:mt-0 md:text-sm md:font-medium">
+            <nav className="bg-white border-gray-200 px-2 sm:px-4 py-2.5 rounded dark:bg-gray-800">
+                <div className="container flex  flex-wrap md:flex-nowrap justify-between items-center mx-auto">
+                    <a href="#" className="flex items-center">
+                        {/*<img src="/docs/images/logo.svg" className="mr-3 h-6 sm:h-10" alt="Flowbite Logo"/>*/}
+                        <span
+                            className="self-center text-xl font-semibold whitespace-nowrap dark:text-white">EcommerceTarea</span>
+                    </a>
+                    <div className="flex md:order-2">
+                        {/*<button type="button"*/}
+                        {/*        className="text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:ring-blue-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center mr-3 md:mr-0 dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800">Get*/}
+                        {/*    started*/}
+                        {/*</button>*/}
+                        <button data-collapse-toggle="mobile-menu-4" type="button"
+                                className="inline-flex items-center p-2 text-sm text-gray-500 rounded-lg md:hidden hover:bg-gray-100 focus:outline-none focus:ring-2 focus:ring-gray-200 dark:text-gray-400 dark:hover:bg-gray-700 dark:focus:ring-gray-600"
+                                aria-controls="mobile-menu-4" aria-expanded="false">
+                            <span className='text-2xl'> <IoMenu/></span>
+                        </button>
+                    </div>
+                    <div className="hidden justify-between items-center w-full md:flex p-3 md:order-1"
+                         id="mobile-menu">
+                        <input type="text" id="email-adress-icon"
+                               className="block p-2 pl-10 w-full text-gray-900 bg-gray-50 rounded-lg border border-gray-300 sm:text-sm focus:ring-blue-500 focus:border-blue-500 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
+                               placeholder="Search..."/>
+                    </div>
+                    <div className="hidden justify-between items-center w-full md:flex md:w-auto md:order-3"
+                         id="mobile-menu-4">
+                        <ul className="flex m-0 flex-col  mt-4 md:flex-row md:space-x-8 md:mt-0 md:text-sm md:font-medium">
                             <li>
-                              <CategoriesMenu/>
+                                <CategoriesMenu/>
                             </li>
                             <li>
                                 <HomeWidget/>
