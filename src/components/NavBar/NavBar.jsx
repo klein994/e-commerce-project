6d62a86f5b90366d62104aa9db0039dce8043298
klein994 <klein478@gmail.com>
2022-03-01 21:04:01 -0300

    logo marca

--- src/components/NavBar/NavBar.jsx
+++ src/components/NavBar/NavBar.jsx
@@ -1,4 +1,5 @@
 import React from 'react';
+import LOGO from '../../logo.svg'
 import CartWidget from "./CartWidget/CartWidget";
 import HomeWidget from "./HomeWidget/HomeWidget";
 import LogInWidget from "./LoginWidget/LogInWidget";
@@ -11,15 +12,11 @@ const NavBar = () => {
             <nav className="bg-white  border-gray-200 px-2 sm:px-4 py-2.5  dark:bg-gray-800 ">
                 <div className="container flex  flex-wrap md:flex-nowrap justify-between items-center mx-auto">
                     <a href="#" className="flex items-center">
-                        {/*<img src="/docs/images/logo.svg" className="mr-3 h-6 sm:h-10" alt="Flowbite Logo"/>*/}
-                        <span
-                            className="self-center text-xl font-semibold whitespace-nowrap dark:text-white">EcommerceTarea</span>
+                        <img src={LOGO} className="mr-3 h-auto max-w-xs sm:h-10" alt="Logo"/>
+
                     </a>
                     <div className="flex md:order-2">
-                        {/*<button type="button"*/}
-                        {/*        className="text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:ring-blue-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center mr-3 md:mr-0 dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800">Get*/}
-                        {/*    started*/}
-                        {/*</button>*/}
+
                         <button data-collapse-toggle="mobile-menu-4" type="button"
                                 className="inline-flex items-center p-2 text-sm text-gray-500 rounded-lg md:hidden hover:bg-gray-100 focus:outline-none focus:ring-2 focus:ring-gray-200 dark:text-gray-400 dark:hover:bg-gray-700 dark:focus:ring-gray-600"
                                 aria-controls="mobile-menu-4" aria-expanded="false">
